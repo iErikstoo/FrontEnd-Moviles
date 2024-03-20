d8641203fdf9ba9d34da1b2885e508f5d8f341bf
crisca807 <crsica807@gmail.com>
2024-03-20 16:47:12 -0500

    realizado hasta montar un servidor node js

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from './src/views/home/home';
-import { RegisterScreen } from './src/views/register/Register';
+import { HomeScreen } from './src/Presentation/views/home/home';
+import { RegisterScreen } from
+'./src/Presentation/views/register/Register'; 
 
 export type RootStackParamList = {
   HomeScreen: undefined,
